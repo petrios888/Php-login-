d6bac1878171c915d7ed49362235851a3f1238c5
petrios888 <appeter22@yahoo.com>
2017-03-15 00:25:09 -0400

    make it better

--- adduser.php
+++ adduser.php
@@ -193,4 +193,216 @@ function fix_string($string)
 	return htmlentites ($string);
 }
 
+
+* Class for executing external commands.
+ *
+ * @category Jyxo
+ * @package Jyxo\Shell
+ * @copyright Copyright (c) 2005-2011 Jyxo, s.r.o.
+ * @license https://github.com/jyxo/php/blob/master/license.txt
+ * @author Ondřej Procházka
+ * @author Matěj Humpál
+ */
+class Client
+{
+	/**
+	 * List of running processes.
+	 *
+	 * @var array
+	 */
+	protected $processList;
+	/**
+	 * Actual working directory.
+	 *
+	 * @var string
+	 */
+	protected $cwd;
+	/**
+	 * Environment properties.
+	 *
+	 * @var array
+	 */
+	protected $env = [];
+	/**
+	 * Stdout output.
+	 *
+	 * @var string
+	 */
+	protected $out;
+	/**
+	 * Stderr output.
+	 *
+	 * @var string
+	 */
+	protected $error;
+	/**
+	 * Constructor.
+	 *
+	 * @param string $cwd Working directory
+	 * @param array $env Array of environment properties
+	 */
+	public function __construct(string $cwd = '', array $env = [])
+	{
+		$this->setCwd($cwd);
+		$this->env = $_ENV;
+		$this->setEnv($env);
+	}
+	/**
+	 * Returns a list of processes.
+	 *
+	 * Works only on Linux.
+	 *
+	 * @return \Jyxo\Shell\Client
+	 */
+	public function loadProcessList(): self
+	{
+		$output = shell_exec('ps aux');
+		$data = explode("\n", $output);
+		foreach ($data as $value) {
+			$value = preg_replace('/ +/', ' ', $value);
+			$list = explode(' ', $value);
+			$commands[$list[10]][] = $list[1];
+		}
+		$this->processList = $commands;
+		return $this;
+	}
+	/**
+	 * Checks if there is a process of the given name.
+	 *
+	 * Works only on Linux.
+	 *
+	 * @param string $name Process name
+	 * @return boolean
+	 */
+	public function processExists(string $name): bool
+	{
+		return array_key_exists($name, $this->processList);
+	}
+	/**
+	 * Kills all processes of the given name.
+	 *
+	 * Works only on Linux.
+	 *
+	 * @param string $name Process name
+	 * @return \Jyxo\Shell\Client
+	 */
+	public function killProcess(string $name): self
+	{
+		shell_exec('killall -s KILL ' . $name);
+		return $this;
+	}
+	/**
+	 * Sets working directory.
+	 *
+	 * Defaults to null.
+	 *
+	 * @param string $cwd Working directory
+	 * @return \Jyxo\Shell\Client
+	 */
+	public function setCwd(string $cwd = ''): Client
+	{
+		$this->cwd = $cwd;
+		return $this;
+	}
+	/**
+	 * Adds one or more environment properties.
+	 *
+	 * @param array $env Array of properties
+	 * @return \Jyxo\Shell\Client
+	 */
+	public function setEnv(array $env): Client
+	{
+		$this->env = array_merge($this->env, $env);
+		return $this;
+	}
+	/**
+	 * Removes environment properties.
+	 *
+	 * @return \Jyxo\Shell\Client
+	 */
+	public function clearEnv(): Client
+	{
+		$this->env = $_ENV;
+		return $this;
+	}
+	/**
+	 * Executes an external command.
+	 *
+	 * Captures stdout and stderr.
+	 * Throws an exception on status code != 0.
+	 *
+	 * @param string $cmd Command to execute
+	 * @param integer $status Status code
+	 * @return \Jyxo\Shell\Client
+	 * @throws \Jyxo\Shell\Exception On execution error
+	 */
+	public function exec(string $cmd, int &$status = null): Client
+	{
+		static $descriptorSpec = [
+			0 => ['pipe', 'r'],
+			1 => ['pipe', 'w'],
+			2 => ['pipe', 'w']
+		];
+		$env = $this->env;
+		if (ini_get('safe_mode')) {
+			// If the safe_mode is set on, we have to check which properties we are allowed to set.
+			$allowedPrefixes = explode(',', ini_get('safe_mode_allowed_env_vars'));
+			$protectedVars = explode(',', ini_get('safe_mode_protected_env_vars'));
+			// Throw away protected properties.
+			$env = array_diff_key($env, array_fill_keys($protectedVars, true));
+			// Throw away properties that do not have the allowed prefix.
+			foreach ($env as $name => $value) {
+				foreach ($allowedPrefixes as $prefix) {
+					// Empty prefix - allow all properties.
+					if ($prefix === '') {
+						break 2;
+					}
+					if (substr($name, 0, strlen($prefix)) == $prefix) {
+						continue 2;
+					}
+				}
+				unset($env[$name]);
+			}
+		}
+		$cmd = (string) $cmd;
+		$process = proc_open($cmd, $descriptorSpec, $pipes, !empty($this->cwd) ? $this->cwd : null, !empty($env) ? $env : null);
+		if (!is_resource($process)) {
+			throw new Exception('Unable to start shell process.');
+		}
+		$this->out = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+		$this->error = stream_get_contents($pipes[2]);
+		fclose($pipes[2]);
+		$status = proc_close($process);
+		if ($status !== 0) {
+			throw new Exception(
+				'Command ' . $cmd . ' returned code ' . $status
+					. '. Output: ' . $this->error
+			);
+		}
+		return $this;
+	}
+	/**
+	 * Returns stdout contents.
+	 *
+	 * @return string
+	 */
+	public function getOut(): string
+	{
+		return $this->out;
+	}
+	/**
+	 * Returns stderr contents.
+	 *
+	 * @return string
+	 */
+	public function getError(): string
+	{
+		return $this->error;
+	}
+}
+
+
+
+
 ?>
